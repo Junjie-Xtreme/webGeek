54052fb4e80ba2ce7c44464f1001c70b792e6d62
Junjie0 <771617640abc@gmail.com>
2025-07-03 23:19:20 -0400

    Make a directory for backend

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,10 @@
+// src/pages/Dashboard.js
+import { Typography, Box } from '@mui/material';
+
+export default function DashboardPage() {
+  return (
+    <Box sx={{ p: 4 }}>
+      <Typography variant="h4">Welcome to Dashboard</Typography>
+    </Box>
+  );
+}
---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,46 @@
+// src/pages/LoginPage.js
+import '../App.css';
+import { Container, Typography, Button, Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+export default function LoginPage() {
+  const navigate = useNavigate();
+  const handleLogin = (e) => {
+    //window.location.href = 'http://localhost:5000/auth/cas/login'; // CAS, dashboard page need redirected by endfront
+    navigate('/dashboard');
+  }
+
+  return (
+    <Box
+      sx={{
+        backgroundImage: 'url(/login-bg.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Container
+        maxWidth="sm"
+        sx={{
+          bgcolor: 'var(--vt-maroon)',
+          p: 4,
+          borderRadius: 2,
+          boxShadow: 3,
+          textAlign: 'center',
+        }}
+      >
+        <Typography variant="h4" color="white" gutterBottom>
+          Welcome to CS 4704 Capstone
+        </Typography>
+
+
+          <Button onClick={handleLogin} variant="contained" color="primary">
+            CS Login
+          </Button>
+      </Container>
+    </Box>
+  );
+}
