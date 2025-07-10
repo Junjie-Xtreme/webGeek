2ce13393caa5d159b35c2c4a5ee59faf7c330070
Junjie0 <771617640abc@gmail.com>
2025-07-10 04:40:54 -0400

    Refactor folder structure and update .gitignore

---
+++ frontend/src/mockStudent.js
@@ -0,0 +1,11 @@
+// src/mock/projects.js
+export const mockStudent = [
+  {
+    id: 1,
+    name: "ken Carson",
+    email: "kcarson@test.edu",
+    skills: ["JavaScript", "React", "Node.js"],
+    preferredProjects: [2, 3],
+    teammatePreferences: ["Bob Lee", "David Chen"],
+  },
+];
