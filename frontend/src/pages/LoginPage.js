804024de87e55bbf0ef4e0459fba989407006bbc
Junjie0 <771617640abc@gmail.com>
2025-07-15 18:12:26 -0400

    Updated 7.15, start here

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -5,8 +5,9 @@ import { useNavigate } from 'react-router-dom';
 
 export default function LoginPage() {
   const navigate = useNavigate();
+
   const handleLogin = (e) => {
-    //window.location.href = 'http://localhost:5000/auth/cas/login'; // CAS, dashboard page need redirected by endfront
+    //window.location.href = 'http://localhost:3001/auth/cas/'; // CAS, dashboard page need redirected by endfront
     navigate('/dashboardstudent');
   }
 
